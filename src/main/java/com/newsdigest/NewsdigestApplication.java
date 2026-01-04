baeae87faf9dc4671e94c6cb3e9e6b90eae449ef
Insaniis <hall3dead@gmail.com>
2026-01-03 22:32:19 -0300

    Initial Spring Boot base structure

---
+++ src/main/java/com/newsdigest/NewsdigestApplication.java
@@ -0,0 +1,13 @@
+package com.newsdigest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsdigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsdigestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsdigest/NewsdigestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsdigest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsdigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
